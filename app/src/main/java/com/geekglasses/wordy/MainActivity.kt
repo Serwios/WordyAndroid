ffd8a1fb8897758904915ef5e69f4c9565171e58
Serwio YK <56473255+Serwios@users.noreply.github.com>
2024-02-04 15:48:26 +0200

    Added keyboard remove

--- app/src/main/java/com/geekglasses/wordy/MainActivity.kt
+++ app/src/main/java/com/geekglasses/wordy/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.geekglasses.wordy
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -51,6 +53,8 @@ class MainActivity : AppCompatActivity() {
                     Constants.MAXIMAL_FRESHNESS
                 )
             )
+
+            hideKeyboard()
         }
 
         quizButton.setOnClickListener {
@@ -68,5 +72,10 @@ class MainActivity : AppCompatActivity() {
 
             startActivity(quizActivity)
         }
+    }
+
+    private fun hideKeyboard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(wordEditText.windowToken, 0)
     }
 }
