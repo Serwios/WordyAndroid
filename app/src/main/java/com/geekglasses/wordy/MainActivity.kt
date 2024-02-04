45a4a8c9636edeaa85b9762873fc0133334880ec
Serwio YK <56473255+Serwios@users.noreply.github.com>
2024-02-04 15:42:15 +0200

    Added clearance for inputs

--- app/src/main/java/com/geekglasses/wordy/MainActivity.kt
+++ app/src/main/java/com/geekglasses/wordy/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.geekglasses.wordy
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var saveWordButton: Button
     private lateinit var quizButton: Button
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,6 +28,16 @@ class MainActivity : AppCompatActivity() {
         translationEditText = findViewById(R.id.translationEditText)
         saveWordButton = findViewById(R.id.saveWordButton)
         quizButton = findViewById(R.id.quizButton)
+
+        wordEditText.setOnTouchListener { _, _ ->
+            wordEditText.text.clear()
+            false
+        }
+
+        translationEditText.setOnTouchListener { _, _ ->
+            translationEditText.text.clear()
+            false
+        }
 
         saveWordButton.setOnClickListener {
             wordEditText.text.clear()
